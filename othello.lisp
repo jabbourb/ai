c3ad668b7b0647dcd6e82707f71cdce498c56a01
Bassam Jabbour <bassam.jabbour@gmail.com>
2011-03-11 16:43:24 +0200

    Some play strategies.
    Random move selection and best next-move (depth 1).

--- othello.lisp
+++ othello.lisp
@@ -1,10 +1,23 @@
 (defconstant board-size 8)
 (defparameter *cell-width* 5)
 
+;{{{1 Low-level primitives
 (defun cell-x (cell)
   (car cell))
 (defun cell-y (cell)
   (cdr cell))
+
+(defmacro with-gensyms (syms &body body)
+  "Bind the given names to gensym-generated symbols"
+  `(let ,(mapcar (lambda (sym) `(,sym (gensym))) syms)
+     ,@body))
+
+(defmacro do-cells((cell) &body body)
+  (with-gensyms(x y)
+    `(dotimes (,x board-size)
+       (dotimes (,y board-size)
+         (let ((,cell (cons ,x ,y)))
+           ,@body)))))
 
 (defun board-cell-at (board pos)
   (aref board (cell-x pos) (cell-y pos)))
@@ -20,6 +33,7 @@
     (setf (board-cell-at board '(4 . 3)) :black)
     board))
 
+;{{{1 Pretty printing
 (defun cell-string (color)
   (case color
     (:black "B")
@@ -37,6 +51,7 @@
         (format stream "~v:@<~a~>|" *cell-width*
                 (cell-string (board-cell-at board (cons x y))))))))
 
+;{{{1 Functions
 (defun horizontal-p (start end)
   "Tests if both cells are in the same row"
   (eql (cell-y start) (cell-y end)))
@@ -117,61 +132,112 @@
          (setf (board-cell-at board pos) color)
          board)))
 
-(defun has-valid-move (board color)
-  (dotimes (x board-size)
-    (dotimes (y board-size)
-      (if (validate-move board (cons x y) color)
-        (return-from has-valid-move t))))
-  nil)
+(defun valid-moves (board color &optional (single nil))
+  (let ((moves nil))
+    (do-cells (cell)
+              (if (validate-move board cell color)
+                (if single
+                  (return-from valid-moves t)
+                  (push cell moves))))
+    moves))
 
-(defun is-game-over (board)
-  (not (or (has-valid-move board :black)
-           (has-valid-move board :white))))
+(defun count-pieces(board)
+  (let ((black 0)
+        (white 0))
+    (dotimes (x board-size cnt)
+      (dotimes (y board-size)
+        (case (board-cell-at board (cons x y))
+          (:black (incf black))
+          (:white (incf white)))))
+    (values black white)))
 
 (defun winner-is (board)
-  (labels ((count-pieces(color)
-             (let ((cnt 0))
-               (dotimes (x board-size cnt)
-                 (dotimes (y board-size)
-                   (when (eql (board-cell-at board (cons x y)) color)
-                     (incf cnt)))))))
+  (multiple-value-bind (black white) (count-pieces board)
+    (cond ((> black white) :black)
+          ((> white black) :white)
+          (t nil))))
 
-    (let ((diff (- (count-pieces :black) (count-pieces :white))))
-      (cond ((plusp diff) :black)
-            ((minusp diff) :white)
-            (t nil)))))
-
-(defmacro while (test &body body)
-  `(do ()
-     ((not ,test))
-     ,@body))
-
+;{{{1 Strategies
 (defmacro do-while-not (vars body test)
   `(do ,(mapcar #'list `,vars)
-     (,test)
+     (,test t)
      ,@body))
 
-(defun othello ()
+(defun human-player(board color)
+  "Ask the player for the move to be taken"
+  (board-print t board)
+  (do-while-not (move)
+                ((format t "~&~A> " color)
+                 (finish-output nil)
+                 (setf move (read)))
+                (and (consp move)
+                     (do-move board move color))))
+
+(defun random-elt(lst)
+  "Choose a random element from a list"
+  (elt lst (random (length lst))))
+
+(defun random-strategy(board color)
+  "A bot that selects a random valid move at each turn"
+  (do-move board
+           (random-elt (valid-moves board color))
+           color))
+
+(defun maximize-strategy(board color)
+  "A bot that selects the best move at each turn by maximizing the
+  difference of pieces to the advantage of the player"
+  (flet ((maximize-difference()
+           (let ((best-move nil)
+                 (best-score 0))
+             (do-cells (cell)
+                       (let ((ends (validate-move board cell color))
+                             (score 0))
+                         (dolist (end ends)
+                           (incf score (length (intermediate-cells cell end))))
+                         (if (> score best-score)
+                           (setf best-move cell
+                                 best-score score))))
+             best-move)))
+
+    (do-move board (maximize-difference) color)))
+
+;{{{1 Main function
+(defun check-and-play(board player color)
+  (and (valid-moves board color t)
+       (funcall player board color)))
+
+(defmacro loop-while (test)
+  `(do ()
+     ((not ,test))
+     nil))
+
+; We can implement this as a function, since we want all the args
+; evaluated in order
+(defun greedy-or (&rest args)
+  (some #'identity args))
+
+(defun othello (p1 p2)
+  "Main function"
   (let ((board (make-board)))
-    (labels ((single-turn(color)
-               (when (has-valid-move board color)
-                 (board-print t board)
-                 (do-while-not (move)
-                               ((format t "~&~A> " color)
-                                (finish-output nil)
-                                (setf move (read)))
-                               (and (consp move)
-                                    (do-move board move color))))))
+    (loop-while (greedy-or (check-and-play board p1 :black)
+                           (check-and-play board p2 :white)))
 
-      (while (not (is-game-over board))
-             (single-turn :black)
-             (single-turn :white))
+    (board-print t board)
+    (terpri)
+    (princ "Game Over!")
+    (let ((winner (winner-is board)))
+      (if (null winner)
+        (princ "It is a tie!")
+        (format t "And the winner is... ~a!" winner))
+      winner)))
 
-      (princ "Game Over!")
-      (let ((winner (winner-is board)))
-        (if (null winner)
-          (princ "It is a tie!")
-          (format t "And the winner is... ~a!" winner))))))
+;{{{1 Test run
+(let ((white 0)
+      (black 0))
+  (dotimes (n 1000)
+    (case (othello #'random-strategy #'maximize-strategy)
+      (:white (incf white))
+      (:black (incf black))))
+  (format t "~&~a:~a~&~a:~a" :black black :white white))
 
-
-(othello)
+; vim: fdm=marker
