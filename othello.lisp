cc12a3d2e6335bedb237a4d339a67f51b828c7bc
Bassam Jabbour <bassam.jabbour@gmail.com>
2011-03-15 20:52:04 +0200

    Minimax algorithm
    This alogrithm allows to unfold the tree of moves up to a certain
    recursion depth, then rewind.
    
    I also moved from Vim to Emacs, so there are also multiple formatting
    modifs.

--- othello.lisp
+++ othello.lisp
@@ -1,7 +1,7 @@
 (defconstant board-size 8)
 (defparameter *cell-width* 5)
 
-;{{{1 Low-level primitives
+;;;; Low-level primitives
 (defun cell-x (cell)
   (car cell))
 (defun cell-y (cell)
@@ -11,8 +11,7 @@
   "Bind the given names to gensym-generated symbols"
   `(let ,(mapcar (lambda (sym) `(,sym (gensym))) syms)
      ,@body))
-
-(defmacro do-cells((cell) &body body)
+(defmacro do-cells ((cell) &body body)
   (with-gensyms(x y)
     `(dotimes (,x board-size)
        (dotimes (,y board-size)
@@ -24,7 +23,7 @@
 (defun (setf board-cell-at) (color board pos)
   (setf (aref board (cell-x pos) (cell-y pos)) color))
 
-(defun make-board()
+(defun make-board ()
   (let ((board (make-array (list board-size board-size)
                            :initial-element nil)))
     (setf (board-cell-at board '(3 . 3)) :white)
@@ -33,7 +32,15 @@
     (setf (board-cell-at board '(4 . 3)) :black)
     board))
 
-;{{{1 Pretty printing
+(defun copy-array (array)
+  (let ((dims (array-dimensions array)))
+    (adjust-array
+     (make-array dims :displaced-to array)
+     dims)))
+(defun copy-board (board)
+  (copy-array board))
+
+;;;; Pretty printing
 (defun cell-string (color)
   (case color
     (:black "B")
@@ -51,7 +58,7 @@
         (format stream "~v:@<~a~>|" *cell-width*
                 (cell-string (board-cell-at board (cons x y))))))))
 
-;{{{1 Functions
+;;;; Functions
 (defun horizontal-p (start end)
   "Tests if both cells are in the same row"
   (eql (cell-y start) (cell-y end)))
@@ -70,23 +77,8 @@
 (defun range (a b)
   "Returns numbers between a and b, exclusive"
   (if (<= a b)
-    (loop for x from (1+ a) to (1- b) collect x)
-    (range b a)))
-
-(defun intermediate-cells (start end)
-  "Returns the list of cells between start and end, exclusive"
-  (cond ((horizontal-p start end)
-         (mapcar (lambda (x) (cons x (cell-y start))) (range (cell-x start) (cell-x end))))
-        ((vertical-p start end)
-         (mapcar (lambda (y) (cons (cell-x start) y)) (range (cell-y start) (cell-y end))))
-        (t
-          (mapcar #'cons (range (cell-x start) (cell-x end)) (range (cell-y start) (cell-y end))))))
-
-(defun flip-cells (board start end color)
-  "Flip board cells between start and end so that they match the specified color"
-  (dolist (c (intermediate-cells start end) board)
-    (if (not (null (board-cell-at board c)))
-      (setf (board-cell-at board c) color))))
+      (loop for x from (1+ a) to (1- b) collect x)
+      (range b a)))
 
 (defun in-bounds (x y)
   "Check if coordinates are inside the board"
@@ -97,30 +89,48 @@
   "Compute the cartesian product of a set with itself"
   (mapcan (lambda (x) (mapcar (lambda (y) (cons x y)) lst)) lst))
 
-(defun validate-move (board pos color)
-  "If the move is valid, return the list of cells that close the bracket in all directions.
-  Else return nil."
-  (let ((!color (color-inv color)))
-    (labels ((valid-move-incr(p step-x step-y acc)
-               (let ((next-x (funcall step-x (cell-x p)))
-                     (next-y (funcall step-y (cell-y p))))
-                 (when (in-bounds next-x next-y)
-                   (let* ((next-cell (cons next-x next-y))
-                          (next-color (board-cell-at board next-cell)))
-                     (or
-                       (and (eql next-color color)
-                            (plusp acc) ;At least one piece of the opposite color
-                            next-cell)
-                       (and (eql next-color !color)
-                            (valid-move-incr next-cell step-x step-y (1+ acc)))))))))
+(defun intermediate-cells (start end)
+  "Returns the list of cells between start and end, exclusive"
+  (cond ((horizontal-p start end)
+         (mapcar (lambda (x) (cons x (cell-y start))) (range (cell-x start) (cell-x end))))
+        ((vertical-p start end)
+         (mapcar (lambda (y) (cons (cell-x start) y)) (range (cell-y start) (cell-y end))))
+        (t
+         (mapcar #'cons (range (cell-x start) (cell-x end)) (range (cell-y start) (cell-y end))))))
 
-      (and (null (board-cell-at board pos))
-           (remove nil (mapcar (lambda (steps) (valid-move-incr pos (car steps) (cdr steps) 0))
-                               (remove (cons #'identity #'identity)
-                                       (cartesian-self (list #'identity
-                                                             (lambda (x) (1+ x))
-                                                             (lambda (x) (1- x))))
-                                       :test #'equal)))))))
+(defun flip-cells (board start end color)
+  "Flip board cells between start and end so that they match the specified color"
+  (dolist (c (intermediate-cells start end) board)
+    (if (not (null (board-cell-at board c)))
+        (setf (board-cell-at board c) color))))
+
+(let ((incr-funcs (remove (cons #'identity #'identity)
+                          (cartesian-self (list #'identity
+                                                (lambda (x) (1+ x))
+                                                (lambda (x) (1- x))))
+                          :test #'equal)))
+
+  (defun validate-move (board pos color &optional (quick nil))
+    "If the move is valid, return the list of cells that close the bracket in all directions.
+    Else return nil."
+    (let ((!color (color-inv color)))
+      (labels ((valid-move-incr(p steps acc)
+                 (let ((next-x (funcall (car steps) (cell-x p)))
+                       (next-y (funcall (cdr steps) (cell-y p))))
+                   (when (in-bounds next-x next-y)
+                     (let* ((next-cell (cons next-x next-y))
+                            (next-color (board-cell-at board next-cell)))
+                       (or
+                        (and (eql next-color color)
+                             (plusp acc) ;At least one piece of the opposite color
+                             next-cell)
+                        (and (eql next-color !color)
+                             (valid-move-incr next-cell steps (1+ acc)))))))))
+
+        (and (null (board-cell-at board pos))
+             (if quick
+                 (some (lambda(steps) (valid-move-incr pos steps 0)) incr-funcs)
+                 (remove nil (mapcar (lambda(steps) (valid-move-incr pos steps 0)) incr-funcs))))))))
 
 (defun do-move (board pos color)
   "Place a piece on the board at the specified position, flipping other
@@ -132,24 +142,34 @@
          (setf (board-cell-at board pos) color)
          board)))
 
-(defun valid-moves (board color &optional (single nil))
+(defun valid-moves (board color)
   (let ((moves nil))
     (do-cells (cell)
-              (if (validate-move board cell color)
-                (if single
-                  (return-from valid-moves t)
-                  (push cell moves))))
+      (if (validate-move board cell color t)
+          (push cell moves)))
     moves))
 
-(defun count-pieces(board)
+(defun has-valid-move (board color)
+  (do-cells (cell)
+    (if (validate-move board cell color t)
+        (return-from has-valid-move t)))
+  nil)
+
+(defun count-pieces (board)
   (let ((black 0)
         (white 0))
-    (dotimes (x board-size cnt)
+    (dotimes (x board-size)
       (dotimes (y board-size)
         (case (board-cell-at board (cons x y))
           (:black (incf black))
           (:white (incf white)))))
     (values black white)))
+
+(defun score (board color)
+  (multiple-value-bind (black white) (count-pieces board)
+    (ecase color
+      (:white (- white black))
+      (:black (- black white)))))
 
 (defun winner-is (board)
   (multiple-value-bind (black white) (count-pieces board)
@@ -157,62 +177,84 @@
           ((> white black) :white)
           (t nil))))
 
-;{{{1 Strategies
-(defmacro do-while-not (vars body test)
-  `(do ,(mapcar #'list `,vars)
-     (,test t)
-     ,@body))
-
-(defun human-player(board color)
+;;;; Strategies
+(defun human-player (board color)
   "Ask the player for the move to be taken"
   (board-print t board)
-  (do-while-not (move)
-                ((format t "~&~A> " color)
-                 (finish-output nil)
-                 (setf move (read)))
-                (and (consp move)
-                     (do-move board move color))))
+  (do ((move))
+      ((and (consp move)
+            (validate-move board move color t))
+       move)
+    (format t "~&~A> " color)
+    (finish-output nil)
+    (setf move (read))))
 
-(defun random-elt(lst)
+(defun random-elt (lst)
   "Choose a random element from a list"
   (elt lst (random (length lst))))
 
-(defun random-strategy(board color)
+(defun random-strategy (board color)
   "A bot that selects a random valid move at each turn"
-  (do-move board
-           (random-elt (valid-moves board color))
-           color))
+  (random-elt (valid-moves board color)))
 
-(defun maximize-strategy(board color)
+(defun maximize-strategy (board color)
   "A bot that selects the best move at each turn by maximizing the
-  difference of pieces to the advantage of the player"
-  (flet ((maximize-difference()
-           (let ((best-move nil)
-                 (best-score 0))
-             (do-cells (cell)
-                       (let ((ends (validate-move board cell color))
-                             (score 0))
-                         (dolist (end ends)
-                           (incf score (length (intermediate-cells cell end))))
-                         (if (> score best-score)
-                           (setf best-move cell
-                                 best-score score))))
-             best-move)))
+difference of pieces to the advantage of the player"
+  (let ((best-move nil)
+        (best-score 0))
+    (do-cells (cell)
+      (let ((ends (validate-move board cell color))
+            (score 0))
+        (dolist (end ends)
+          (incf score (length (intermediate-cells cell end))))
+        (if (> score best-score)
+            (setf best-move cell
+                  best-score score))))
+    best-move))
 
-    (do-move board (maximize-difference) color)))
+(defun minimax-searcher (ply score)
+"Build a bot that, when given a board and a color, will unfold the
+tree of moves up to a certain depth specified by *ply*, evaluate end
+configurations using *score* and rewind the tree by applying a minimax
+algorithm"
+  (labels ((select-move (moves comparator)
+             (let ((best (first moves)))
+               (dolist (move (rest moves) best)
+                 (if (funcall comparator (cdr move) (cdr best))
+                     (setf best move)))))
 
-;{{{1 Main function
-(defun check-and-play(board player color)
-  (and (valid-moves board color t)
-       (funcall player board color)))
+           (minimax-turn (board orig-color move-color ply)
+             (if (zerop ply)
+                 (cons nil (funcall score board orig-color))
+                 (let ((scores
+                        (mapcar (lambda (move)
+                                  (cons move (cdr (minimax-turn (do-move (copy-board board) move move-color)
+                                                                orig-color (color-inv move-color) (1- ply)))))
+                                (valid-moves board move-color))))
+                   (if (null scores)
+                                        ;No valid moves left
+                       (cons nil (funcall score board orig-color))
+                       (if (eql orig-color move-color)
+                           (select-move scores #'>)
+                           (select-move scores #'<)))))))
+
+    (lambda (board color)
+      (car (minimax-turn board color color ply)))))
+
+;;;; Main function
+(defun check-and-play (board player color)
+  (and (has-valid-move board color)
+       (do-move board
+         (funcall player board color)
+         color)))
 
 (defmacro loop-while (test)
   `(do ()
-     ((not ,test))
-     nil))
+       ((not ,test))
+    nil))
 
-; We can implement this as a function, since we want all the args
-; evaluated in order
+;; We can implement this as a function, since we want all the args
+;; evaluated in order
 (defun greedy-or (&rest args)
   (some #'identity args))
 
@@ -227,17 +269,8 @@
     (princ "Game Over!")
     (let ((winner (winner-is board)))
       (if (null winner)
-        (princ "It is a tie!")
-        (format t "And the winner is... ~a!" winner))
+          (princ "It is a tie!")
+          (format t "And the winner is... ~a!" winner))
       winner)))
 
-;{{{1 Test run
-(let ((white 0)
-      (black 0))
-  (dotimes (n 1000)
-    (case (othello #'random-strategy #'maximize-strategy)
-      (:white (incf white))
-      (:black (incf black))))
-  (format t "~&~a:~a~&~a:~a" :black black :white white))
-
-; vim: fdm=marker
+(othello #'maximize-strategy (minimax-searcher 4 #'score))
